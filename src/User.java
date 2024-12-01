1e032384e6cc886f88891a1c017792f1be1a3509
Fábio Vieira <fabio.gv003@gmail.com>
2024-12-01 18:55:22 -0300

    Updates img

--- src/User.java
+++ src/User.java
@@ -4,36 +4,32 @@ import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.Statement;
 
-public class User {
-	public String nome="";														1
-	public boolean result = false;
-
+public class User {																1
 	public Connection conectarBD() {											2
-		Connection conn = null;													3
-		try {																	4
-			Class.forName("com.mysql.Driver.Manager").newInstance();			5
+		Connection conn = null;
+		try {																	3
+			Class.forName("com.mysql.Driver.Manager").newInstance();
 			String url = "jdbc:mysql://127.0.0.1/test?user=lopes&password=123";
 			conn = DriverManager.getConnection(url);
-		} 6 catch (Exception e)7 { }8
-		return conn;}															9
-
-	public boolean verificarUsuario (String login, String senha) {				10
-		String sql = "";														11
+		} 4 catch (Exception e) { }5
+		return conn;}															6
+	public String nome="";
+	public boolean result = false;
+	public boolean verificarUsuario (String login, String senha) {				7
+		String sql = "";
 		sql += "select nome from usuarios ";
 		sql += "where login = " + "'" + login + "'";
 		sql += "  and senha = " + "'" + senha + "';";
-		Connection conn = conectarBD();											12
-		try {																	13
-			Statement st = conn.createStatement();								14
+		Connection conn = conectarBD();
+		try {																	8
+			Statement st = conn.createStatement();
 			ResultSet rs = st.executeQuery(sql);
-			if(rs.next()) {														15
-				result = true;													16
-				nome = rs.getString("nome");
-			}																	17
-		}18	catch (Exception e) 19{ }20
-		return result;
-	}																			21
-
-
-	}//fim da class22
+			if(rs.next()) {														9
+				result = true;
+				nome = rs.getString("nome");									10
+			}
+		}11	catch (Exception e) { }12
+		return result;13
+	}																			14
+}//fim da class22
 
